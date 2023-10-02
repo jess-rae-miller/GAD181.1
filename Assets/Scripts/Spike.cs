8439b83bef2279fe29d8e49cf11ede5f5f887616
sam-murrandah <sammurrandah@gmail.com>
2023-10-02 22:04:04 +1100

    fix spike

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float scalingTime = 2.0f; // Time it takes to scale from 0 to the saved Y-axis value
+    private float scaleStartTime;
+    private bool isScaling = false;
+    private Vector3 initialScale;
+
+    private void Start()
+    {
+        scaleStartTime = Time.time;
+        initialScale = transform.localScale;
+    }
 
     private void Update()
     {
-        Vector3 movementDirection = Vector3.down * speed * Time.deltaTime;
-        transform.Translate(movementDirection, Space.World);
+        if (!isScaling && Time.time - scaleStartTime < scalingTime)
+        {
+            // Calculate the current scale factor based on time
+            float scaleFactor = (Time.time - scaleStartTime) / scalingTime;
+            
+            // Set the local scale of the object to the calculated scale factor
+            transform.localScale = new Vector3(initialScale.x, scaleFactor * initialScale.y, initialScale.z);
+        }
+        else if (!isScaling)
+        {
+            // Ensure that the scale reaches exactly the initial Y-axis value at the end
+            transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+            isScaling = true;
+        }
     }
 }
